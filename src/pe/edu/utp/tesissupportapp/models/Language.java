ee324801f0944c27966804f38cfe7983aa1bb5ee
ISANCHEZSANCHO <1411797@utp.edu.pe>
2017-06-16 17:19:17 -0500

    crear modelo languages

---
+++ src/pe/edu/utp/tesissupportapp/models/Language.java
@@ -0,0 +1,68 @@
+package pe.edu.utp.tesissupportapp.models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by USER on 16/06/2017.
+ */
+public class Language {
+
+    private int id;
+    private String name;
+    private String description;
+
+    public Language(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Language() {
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIdAsString() {
+        return String.valueOf(getId());
+    }
+
+    public Language setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Language setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Language setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public static Language build(ResultSet resultSet) {
+        try {
+            return (new Language())
+                    .setId(resultSet.getInt("language_id"))
+                    .setName(resultSet.getString("language_name"))
+                    .setName(resultSet.getString("language_description"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
